75d7ee5c14893abedf9f8814726d313bd7e6e56f
Borl1seTrunov <borl1se021@icloud.com>
2025-01-17 20:31:16 +0300

    add Stack on array and add tests

---
+++ Stack_on_array/stack_on_array.py
@@ -0,0 +1,30 @@
+from typing import Any
+
+class Stack:
+    def __init__(self) -> None:
+        self.items = []
+        self.size = 0
+
+    """метод добавления элемента в стек"""
+    def push(self, val : Any) -> None:
+        if val is None:
+            return
+        self.items.append(val)
+        self.size += 1
+    
+    """метод удаления элемента из стека"""
+    def pop(self) -> None:
+        if self.size == 0:
+            return
+        self.items.pop()
+        self.size -= 1
+
+    """метод получения значения верхнего элемента"""
+    def check_top(self) -> Any | None:
+        if self.size == 0:
+            return
+        return self.items[-1]
+
+    """метод получения количества элементов в стеке"""
+    def get_size(self) -> int:
+        return self.size
